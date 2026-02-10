cbd80663b8f5ed072c706b84138e3a09776ca158
Mudigonda Veena <veena.mudigonda123@gmail.com>
2026-02-11 05:04:45 +0530

    folder

---
+++ myinfra/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "rg-ubuntu-demo"
+}
+
+variable "vm_name" {
+  default = "ubuntu-vm1"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
